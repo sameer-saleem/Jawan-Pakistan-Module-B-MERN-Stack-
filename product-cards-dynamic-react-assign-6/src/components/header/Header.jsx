51f3a8bb68c6850a35ebc52bfb704028ee591882
Sameer Saleem <saleemsameer736@gmail.com>
2025-10-06 01:41:35 +0500

    food delivery app basic template

--- product-cards-dynamic-react-assign-6/src/components/header/Header.jsx
+++ product-cards-dynamic-react-assign-6/src/components/header/Header.jsx
@@ -1,17 +1,8 @@
-import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
-import AdbIcon from '@mui/icons-material/Adb';
+import { Link } from 'react-router-dom';
 
 function Header() {
 
@@ -19,23 +10,22 @@ function Header() {
         <AppBar position="static">
             <Container maxWidth="xl">
                 <Toolbar>
-                    <Typography
-                        variant="h5"
-                        noWrap
-                        component="a"
-                        href="#app-bar-with-responsive-menu"
-                        sx={{
-                            mr: 2,
-                            flexGrow: 1,
-                            fontFamily: 'monospace',
-                            fontWeight: 700,
-                            letterSpacing: '.3rem',
-                            color: 'inherit',
-                            textDecoration: 'none',
-                        }}
-                    >
-                        Dynamic Products
-                    </Typography>
+                    <Link to="/" className='link'>
+                        <Typography
+                            variant="h5"
+                            noWrap
+                            sx={{
+                                mr: 2,
+                                fontWeight: 700,
+                                letterSpacing: '.2rem',
+                                color: 'white',
+                                textDecoration: 'none'
+                                
+                            }}
+                        >
+                            Dynamic Products
+                        </Typography>
+                    </Link>
                 </Toolbar>
             </Container>
         </AppBar>
--- product-cards-dynamic-react-assign-6/src/components/main-content/MainContent.jsx
+++ product-cards-dynamic-react-assign-6/src/components/main-content/MainContent.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react"
 import ProductCard from "../product-card/ProductCard"
 import Grid from '@mui/material/Grid';
 import Container from "@mui/material/Container";
+import { Route, Routes } from "react-router-dom";
 import ProductDetailPage from "../product-card-detail/ProductCardDetail";
 
 const MainContent = () => {
@@ -13,19 +14,28 @@ const MainContent = () => {
             .then(data => setProductsData(data));
     }, []);
 
+    const Cards = () => {
+        return (
+             <Grid container spacing={2}>
+                {productsData && productsData.map((item, i) => {
+                    const { id, image, title, price, category, description } = item;
+                    return (
+                        <Grid size={{ xs: 12, sm: 6, md: 4, lg: 3 }} key={i} >
+                            <ProductCard id={id} image={image} title={title} price={price} category={category} description={description} productLink={`products/${id}`}/>
+                        </Grid>
+                    )
+                })}
+            </Grid>
+        )
+    }
+
     return (
         <main className="main-content">
             <Container>
-                <Grid container spacing={2}>
-                    {productsData && productsData.map((item, i) => {
-                        const { id, image, title, price, category, description } = item;
-                        return (
-                            <Grid size={{ xs: 12, sm: 6, md: 4, lg: 3 }} key={i}>
-                                <ProductCard id={id} image={image} title={title} price={price} category={category} description={description} />
-                            </Grid>
-                        )
-                    })}
-                </Grid>
+                <Routes>
+                    <Route path="/" element={<Cards />} />
+                    <Route path="products/:productId" element={<ProductDetailPage />} />
+                </Routes>
             </Container>
         </main>
     )
