e7c5c4912cd6bd21ff978f5405d1a548b73e668e
Sameer-Saleem <61308007+sameer-saleem@users.noreply.github.com>
2025-12-06 13:49:15 +0500

    node js 2nd class

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,4 +1,35 @@
 // console.log('hellow world');
 
+// this is for importing main.js file
+// const main = require('./main');
+// console.log(main.hellow());
+// console.log(main.hellow2());
+
+// Class 2 work
 const main = require('./main');
-console.log(main());
+
+// console.log(main, 'main');
+
+const http = require('http');
+// console.log(http);
+
+// console.log(main.readHtmlFile(), 'readHtmlFile');
+
+const server = http.createServer((req, res) => {
+    // console.log(req);
+    main.readHtmlFile((err, data) => {
+        if (err) {
+            // If there's an error, return a 500 status code and an error message
+            res.statusCode = 500;
+            res.end('Error reading HTML file');
+        } else {
+            // If successful, return the HTML content with a 200 status code
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'text/html');
+            res.end(data);  // Send the HTML file content to the client
+        }
+    });       
+    
+});
+
+server.listen(3000);
--- nodejs/main.js
+++ nodejs/main.js
@@ -1,11 +1,35 @@
-// module.exports = () => {
-//     // return "hellow return";
-
-//     for(let i = 0; i < 10; i++ ) {
-//         console.log(i);
-//     }
+// function hellow() {
+//     return 'hellow function';
 // }
 
 
-let x = 10;
-x + 5
+// function hellow2() {
+//     for(let i = 0; i < 10; i++ ) {
+//         console.log(i);
+//     }
+
+//     return 'hellow2 function';
+// }
+
+const fs = require('fs');
+const path = require('path');
+
+function readHtmlFile(callback) {
+
+    const htmlFilePath = path.join(__dirname, 'index.html');
+    
+    fs.readFile(htmlFilePath, (err, data) => {
+        if (err) {
+            return callback('Error reading HTML file:', err);
+        } else {
+            return callback(null, data);
+        }
+    });
+
+}
+
+module.exports = { readHtmlFile };
+
+
+// let x = 10;
+// x + 5
